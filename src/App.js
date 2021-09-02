597d7d8e225ceeb35dbb55130216995bf9aae0c5
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-09-01 20:11:37 -0400

    Redid links for navigation, still not working correctly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
+import Bio from './components/Bio';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+
 
 function App() {
   return (
-    <div>
-        <Navigation />
 
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route path='/' component={Bio} />
+          <Route path='/portfolio' component={Portfolio} />
+          <Route path='/resume' component={Resume} />
+          <Route path='/contact' component={Contact} />
+        </Switch>
         <Footer />
-    </div>
+      </BrowserRouter>
+        
+
+        
+
 
   );     
 }
---
+++ src/components/Bio.js
@@ -0,0 +1,11 @@
+
+
+function Bio() {
+    return (
+      <div>
+          <h1>My Bio</h1>
+      </div>
+    );
+  }
+  
+  export default Bio;
