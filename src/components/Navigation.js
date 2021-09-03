7eda245eaccd86c5f4eabc9bf5ff03b39606cd50
Fernando Zaldivar <f.zaldivar@live.com>
2021-09-03 05:50:19 -0400

    removed the Bootstrap component from Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,15 +6,15 @@ import Navbar from 'react-bootstrap/Navbar'
 function Navigation() {
     return (
         <div className="nav-link-container">
-            <Navbar>
+            <nav>
                     <span className="myName"><h1>Fernando Zaldivar</h1></span>
-                    <div className="nav-link-header ml-auto">
+                    <div className="nav-link-header">
                         <NavLink className="nav-links" to="/">Bio</NavLink>
                         <NavLink className="nav-links" to="/portfolio">Portfolio</NavLink>
                         <NavLink className="nav-links" to="/resume">Resume</NavLink>
                         <NavLink className="nav-links" to="/contact">Contact</NavLink>
                     </div>
-            </Navbar>
+            </nav>
         </div>
     );  
 }
