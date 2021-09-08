3235690ba1d8e57e7b98fdfc66b1c696ffba95ac
Fernando Zaldivar <f.zaldivar@live.com>
2021-09-08 04:25:14 -0400

    Fixed spelling error

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -12,7 +12,7 @@ function Bio() {
               <div className='col center-content'>
                 <p>
                     Bilingual Junior Web Developer that is looking to learn new techniques and use creative solutions for modern day problems.
-                    Recieved a certificate in Full Stack from University of Miami. My major strength lies in working in teams.
+                    Received a certificate in Full Stack from University of Miami. My major strength lies in working in teams.
                     I have 4 years in management experience so being able to delegate tasks and evaluate team strengths and weakness
                     is a skill I am proficient in. Even if not part of a team I can still excel on my own and provide results. 
                 </p>
