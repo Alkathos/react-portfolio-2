1a90b7fecfb537cbc5aeb97dd6a86d7460e5151a
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-10-06 10:53:33 -0400

    Updated Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -11,7 +11,7 @@ function Bio() {
               </div>
               <div className='col center-content'>
                 <p>
-                    Bilingual Junior Web Developer that is looking to learn new techniques and use creative solutions for modern day problems.
+                    Bilingual Junior Front End Web Developer that is looking to learn new techniques and use creative solutions for modern day problems.
                     Received a certificate in Full Stack from University of Miami. My major strength lies in working in teams.
                     I have 4 years in management experience. I am capable of leading many individuals from all walks of life.
                     I excel in problem-solving, emotional intelligence and accountability. I work well individually as well as on a team; adaptable and collaborative. 
