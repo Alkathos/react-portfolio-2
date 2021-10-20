3278cb3194b078f3f47bdb8160fd8130f5d343c6
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-10-20 13:41:35 -0400

    Updated fonts

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,14 +3,13 @@ import Me from '../img/me.jpg'
 function Bio() {
     return (
       <div className="container">
-          <h1>Bio</h1>
           <div className='container'>
             <div className='row'>
               <div className='col'>
                 <img alt="Fernando Zaldivar" src={Me} className='my-photo' />
               </div>
               <div className='col center-content'>
-                <p>
+                <p id="bio">
                     Bilingual Junior Front End Web Developer that is looking to learn new techniques and use creative solutions for modern day problems.
                     Received a certificate in Full Stack from University of Miami. My major strength lies in working in teams.
                     I have 4 years in management experience. I am capable of leading many individuals from all walks of life.
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,12 +6,12 @@ function Navigation() {
     return (
         <div className="nav-link-container">
             <nav>
-                    <span><div className="myName">Fernando Zaldivar</div></span>
+                    <span><div className="myName">FERNANDO ZALDIVAR</div></span>
                     <div className="nav-link-header">
-                        <NavLink className="nav-links" to={process.env.PUBLIC_URL + '/'}>Bio</NavLink>
-                        <NavLink className="nav-links" to={process.env.PUBLIC_URL + '/projects'}>Projects</NavLink>
+                        <NavLink className="nav-links" to={process.env.PUBLIC_URL + '/'}>About</NavLink>
+                        <NavLink className="nav-links" to={process.env.PUBLIC_URL + '/projects'}>Portfolio</NavLink>
                         <NavLink className="nav-links" to={process.env.PUBLIC_URL + '/resume'}>Resume</NavLink>
-                        <NavLink className="nav-links" to={process.env.PUBLIC_URL + '/contact'}>Contact</NavLink>
+                        <NavLink className="nav-links" to={process.env.PUBLIC_URL + '/contact'}>Contact Me</NavLink>
                     </div>
             </nav>
         </div>
